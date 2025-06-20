da40602331e17d2ff5404683769cc2cd198b774a
PiaLeonor12 <jorgeadrian@hotmail.com>
2025-06-20 12:29:33 -0300

    ordeno las operacion de forma descendente-2

--- PH/src/main/java/balance/repository/ClosingDepositRepository.java
+++ PH/src/main/java/balance/repository/ClosingDepositRepository.java
@@ -1,15 +1,24 @@
 package balance.repository;
-
 import balance.model.ClosingDeposit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface ClosingDepositRepository extends JpaRepository<ClosingDeposit, Long> {
+
+     List<ClosingDeposit> findByDepositDateBetweenOrderByDepositDateDesc(LocalDate startDate, LocalDate endDate);
+    List<ClosingDeposit> findByStoreIdOrderByDepositDateDesc(Long storeId);
+    List<ClosingDeposit> findByDepositDateBetweenAndStoreIdOrderByDepositDateDesc(LocalDate startDate, LocalDate endDate, Long storeId);
+  
+    @Query("SELECT c FROM ClosingDeposit c ORDER BY c.depositDate DESC")
+    List<ClosingDeposit> findAllOrderByDepositDateDesc();
+
     List<ClosingDeposit> findByDepositDateBetween(LocalDate startDate, LocalDate endDate);
-    List<ClosingDeposit> findByStoreId(Long storeId);  // <<-- Filtro por ID de store
+    List<ClosingDeposit> findByStoreId(Long storeId); 
     List<ClosingDeposit> findByDepositDateBetweenAndStoreId(LocalDate startDate, LocalDate endDate, Long storeId);
 }
